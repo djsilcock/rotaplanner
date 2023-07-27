0787976d9ec12588d000125787145518f322593f
djsilcock <djsilcock@users.noreply.github.com>
2023-07-27 16:08:19 +0000

    .

--- backend/datastore.py
+++ backend/datastore.py
@@ -1,35 +1,43 @@
+"Data storage class"
+
 from dataclasses import asdict
 from typing import Iterable
 from warnings import warn
 from datetime import date, timedelta
 from datatypes import SessionDuty
-import storage as default_storage
+import storage.filesystem
 
-storages={'default':default_storage}
+storages = {'filesystem': storage.filesystem}
+
 
 class DataStore:
-    def __init__(self, storage='default'):
+    "datastore class"
+
+    def __init__(self, storage_type='filesystem'):
         self.data: dict[tuple, SessionDuty] = {}
         self.dates = []
         self.sessions = ('am', 'pm', 'eve', 'night')
-        self.storage = storage
+        self.storage = storage_type
         self.config = {}
 
     @property
-    def names(self):
-        return self.config.get(None,{}).get('names',())
-    
+    def names(self) -> tuple[str]:
+        "names of staff members"
+        return self.config.get(None, {}).get('names', ())
+
     @property
-    def pubhols(self):
-        return tuple(self.config.get(None,{}).get('pubhols',()))
-    
+    def pubhols(self) -> tuple[date]:
+        "get tuple of public holidays"
+        return tuple(self.config.get(None, {}).get('pubhols', ()))
+
     def get_storage_class(self):
+        "get current storage backend"
         return storages[self.storage]
-    
-    def update_config(self,config:dict,overwrite=False):
+
+    def update_config(self, config: dict, overwrite=False):
         "update configuration information"
         if overwrite:
-            self.config=config.copy()
+            self.config = config.copy()
         else:
             self.config.update(config)
 
@@ -39,7 +47,8 @@ class DataStore:
 
         if overwrite:
             self.data.clear()
-        self.data.update({key:val if isinstance(val,SessionDuty) else SessionDuty(**val) for key,val in data.items()})
+        self.data.update({key: val if isinstance(val, SessionDuty)
+                         else SessionDuty(**val) for key, val in data.items()})
         mindate = min((key[1] for key in self.data),
                       default=date.today())
         maxdate = max((key[1] for key in self.data),
@@ -52,6 +61,7 @@ class DataStore:
                     self.data.setdefault((name, day, sess), SessionDuty())
 
     def get_config(self):
+        "return configuration options as dict"
         return {
             'names': self.names,
             'minDate': min(self.dates).isoformat(),
@@ -61,43 +71,48 @@ class DataStore:
         }
 
     def as_dict(self):
+        "return datastore as JSON-serializable data"
         data = {}
         for (name, day, sess), sessionduty in self.data.items():
             data[f'{day.isoformat()[0:10]}|{name}|{sess}'] = asdict(sessionduty)
         return data
 
-    def setduty(self, name, d, session, duty):
+    def setduty(self, name, duty_date, session, duty):
         "Set duty as determined by menu"
-        cell = self.data.get((name, date.fromisoformat(d), session))
+        cell = self.data.get((name, date.fromisoformat(duty_date), session))
         if isinstance(cell, SessionDuty):
             cell.duty = duty
         else:
             raise TypeError('not a dutycell instance')
 
-    def setflag(self, name, d, session, flag, mutex=()):
+    def setflag(self, name, duty_date, session, flag, mutex=()):
+        "set flag for duty"
         try:
-            cell = self.data[(name, d, session)]
+            cell = self.data[(name, duty_date, session)]
         except KeyError:
             return
         cell.flags.add(flag)
         cell.flags.difference_update(mutex)
 
-    def delflag(self, name, d, session, flag):
+    def delflag(self, name, duty_date, session, flag):
+        "remove flag for duty"
         try:
-            cell = self.data[(name, d, session)]
+            cell = self.data[(name, duty_date, session)]
             cell.flags.discard(flag)
         except KeyError:
             return
 
-    def toggleflag(self, name, d, session, flag):
+    def toggleflag(self, name, duty_date, session, flag):
+        "toggle flag for duty"
         try:
-            cell = self.data[(name, d, session)]
+            cell = self.data[(name, duty_date, session)]
             cell.flags.symmetric_difference_update((flag,))
         except KeyError:
             return
 
-    def set_public_holiday(self,_date,is_holiday=True):
-        pubhols:set[date]=self.config.setdefault('pubhols',set())
+    def set_public_holiday(self, _date, is_holiday=True):
+        "set or unset public holiday"
+        pubhols: set[date] = self.config.setdefault('pubhols', set())
         if is_holiday:
             pubhols.add(_date)
         else:
@@ -110,12 +125,12 @@ class DataStore:
     def load_data(self):
         "load from file"
         try:
-            newfile=self.get_storage_class().load_data()
-            self.data=newfile.data
-            self.config=newfile.config
+            newfile = self.get_storage_class().load_data()
+            self.data = newfile.data
+            self.config = newfile.config
         except FileNotFoundError:
             warn("Savefile not found")
 
     def import_clw_csv(self, csvfile: Iterable[str]):
         "import from csv file"
-        self.update_data(self.storage.import_clw_csv(csvfile))
+        self.update_data(self.get_storage_class().import_clw_csv(csvfile))
--- backend/datatypes.py
+++ backend/datatypes.py
@@ -1,23 +1,17 @@
 "various datatypes"
 from dataclasses import dataclass, field
-from enum import StrEnum
-
-class DutyFlag(StrEnum):
-    "flags for duties"
-    LOCUM='locum'
-    LOCKED='locked'
 
 @dataclass
 class SessionDuty:
     "Session information"
     duty: str|None = None
-    flags: set[DutyFlag]=field(default_factory=set)
+    flags: set[str]=field(default_factory=set)
 
     def __str__(self):
         if not self.duty:
             return ""
         representations={
-            DutyFlag.LOCUM:'£',
-            DutyFlag.LOCKED:'🔒'
+            'locum':'£',
+            'locked':'🔒'
         }
         return f'{self.duty} {"".join(representations[f] for f in self.flags)}'
