62d5084509a3d10ac7f93b56be740c4dc77329c0
djsilcock <djsilcock@users.noreply.github.com>
2024-08-16 20:13:03 +0000

    refactoring to use pydantic

--- py_rotaplanner/templating0.py
+++ py_rotaplanner/templating0.py
@@ -1,9 +1,12 @@
 
 import datetime
 from dataclasses import dataclass,replace,field,is_dataclass,fields
+from pydantic import BaseModel,Field,RootModel
 from enum import StrEnum,IntEnum,auto
-from typing import cast,TYPE_CHECKING
+from typing import cast,TYPE_CHECKING,Annotated,Literal
 import itertools
+import uuid
+import json
 
 if TYPE_CHECKING:
     from scheduling import Activity
@@ -11,7 +14,7 @@ if TYPE_CHECKING:
 
 from ortools.sat.python import cp_model
 
-from typing import List
+from typing import List,Union
 from typing import Optional
 import datetime
 from enum import StrEnum
@@ -25,35 +28,6 @@ from sqlalchemy.orm import relationship
 class Base(DeclarativeBase):
     pass
 
-class RuleBase(Base):
-    __tablename__="rule"
-    id:Mapped[int]=mapped_column(primary_key=True)
-    type: Mapped[str]
-    parent_id:Mapped[int|None]=mapped_column(ForeignKey('rule.id'))
-    parent:Mapped["RuleGroup"]=relationship(back_populates="members",foreign_keys=[parent_id],remote_side=[id])
-    __mapper_args__ = {
-        "polymorphic_identity": "rule",
-        "polymorphic_on":"type"
-    }
-    @classmethod
-    def from_json(cls,data):
-        ruleclass=rule_types[data['type']]
-        if cls!=ruleclass:
-            return ruleclass.from_json(data)
-        return cls(**data)
-
-class Rule(RuleBase):
-    anchor_date:Mapped[datetime.datetime|None]
-    @classmethod
-    def from_json(cls,data):
-        data['anchor_date']=datetime.datetime.fromisoformat(data['anchor_date'])
-        return super().from_json(data)
-
-class GroupType(StrEnum):
-    AND='and'
-    OR='or'
-    NOT='not'
-
 class RuleType(StrEnum):
     "Date rule types"
     DAILY='daily'
@@ -65,12 +39,18 @@ class RuleType(StrEnum):
     GROUP='group'
 
 
-class RuleGroup(RuleBase):
-    group_type:Mapped[str|None]
-    members:Mapped[list[RuleBase]]=relationship(back_populates="parent",cascade='save-update,merge,delete')
-    __mapper_args__ = {
-        "polymorphic_identity": "group",
-    }
+
+class GroupType(StrEnum):
+    AND='and'
+    OR='or'
+    NOT='not'
+
+
+
+class RuleGroup(BaseModel):
+    rule_type:Literal[RuleType.GROUP]=RuleType.GROUP
+    group_type:GroupType
+    members:list['Rule']=Field(default_factory=list)
     def matches(self,match_date):
         match self.group_type:
             case GroupType.AND:
@@ -79,53 +59,41 @@ class RuleGroup(RuleBase):
                 return any(rule.matches(match_date) for rule in self.members)
             case GroupType.NOT:
                 return not any(rule.matches(match_date)for rule in self.members)
-    @classmethod
-    def from_json(cls,data):
-        data['members']=[Rule.from_json(d) for d in data['members']]
-        return super().from_json(data)
 
-class DailyRule(Rule):
-    day_interval:Mapped[int|None]
-    __mapper_args__ = {
-        "polymorphic_identity": RuleType.DAILY,
-    }
+class DailyRule(BaseModel):
+    rule_type:Literal[RuleType.DAILY]=RuleType.DAILY
+    day_interval:int
+    anchor_date:datetime.date
     def matches(self,match_date:datetime.date):
         return (match_date-self.anchor_date).days%self.day_interval==0
 
-
-class WeeklyRule(Rule):
+class WeeklyRule(BaseModel):
     "every nth week"
-    week_interval:Mapped[int|None]
-    __mapper_args__ = {
-        "polymorphic_identity": RuleType.WEEKLY,
-    }
+    rule_type:Literal[RuleType.WEEKLY]=RuleType.WEEKLY
+    week_interval:int
+    anchor_date:datetime.date
     def matches(self,match_date:datetime.date):
         return (match_date-self.anchor_date).days%(7*self.week_interval)==0
 
 
-class MonthlyRule(Rule):
-    "every nth month"
-    month_interval:Mapped[int|None]    
-    __mapper_args__ = {
-        "polymorphic_identity": "monthly",
-    }
+ 
 
-class DayOfMonthRule(MonthlyRule):
+class DayOfMonthRule(BaseModel):
     "nth day of every month"
-    __mapper_args__ = {
-        "polymorphic_identity": RuleType.MONTHLY,
-    }
+    rule_type:Literal[RuleType.MONTHLY]=RuleType.MONTHLY
+    month_interval:int
+    anchor_date:datetime.date
     def matches(self,match_date:datetime.date):
         if self.anchor_date.day!=match_date.day:
             return False
         return ((self.anchor_date.year*12+self.anchor_date.month)-(match_date.year*12+match_date.month))%self.month_interval==0
     
 
-class WeekInMonthRule(MonthlyRule):
+class WeekInMonthRule(BaseModel):
     "the nth week of every mth month"
-    __mapper_args__ = {
-        "polymorphic_identity": RuleType.WEEK_IN_MONTH,
-    }
+    rule_type:Literal[RuleType.WEEK_IN_MONTH]=RuleType.WEEK_IN_MONTH
+    month_interval:int
+    anchor_date:datetime.date
     def matches(self,match_date:datetime.date):
         if match_date.weekday()!=self.anchor_date.weekday():
             return False
@@ -138,84 +106,50 @@ class DateType(StrEnum):
     INCLUSIVE='incl'
     EXCLUSIVE='excl'
 
-class DateRangeRule(RuleBase):
-    start_date:Mapped[datetime.datetime|None]
-    finish_date:Mapped[datetime.datetime|None]
-    range_type:Mapped[str|None]
-    __mapper_args__={
-        "polymorphic_identity":RuleType.DATE_RANGE
-    }
+class DateRangeRule(BaseModel):
+    rule_type:Literal[RuleType.DATE_RANGE]=RuleType.DATE_RANGE
+    start_date:datetime.date
+    finish_date:datetime.date
+    range_type:DateType
     def matches(self,match_date:datetime.date):
         if self.range_type==DateType.INCLUSIVE:
             return self.start_date<=match_date<=self.finish_date
         return not (self.start_date<=match_date<=self.finish_date)
 
 
-class DateTagsRule(Rule):
-    label:Mapped[str|None]
-    date_type:Mapped[str|None]
-    __mapper_args__={
-        "polymorphic_identity":RuleType.DATE_TAGS
-    }
+class DateTagsRule(BaseModel):
+    rule_type:Literal[RuleType.DATE_TAGS]=RuleType.DATE_TAGS
+    label:str
+    date_type:DateType
+
+Rule=Annotated[Union[RuleGroup,DailyRule,WeeklyRule,DayOfMonthRule,WeekInMonthRule,DateRangeRule,DateTagsRule,RuleGroup],Field(discriminator='rule_type')]
 
 
 
-rule_types={
-    RuleType.DAILY:DailyRule,
-    RuleType.WEEKLY:WeeklyRule,
-    RuleType.MONTHLY:MonthlyRule,
-    RuleType.WEEK_IN_MONTH:WeekInMonthRule,
-    RuleType.DATE_RANGE:DateRangeRule,
-    RuleType.DATE_TAGS:DateTagsRule,
-    RuleType.GROUP:RuleGroup
-}
 
-
-class Template:
-    __tablename__ = "template"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    type: Mapped[str]
-    ruleset:Mapped[RuleGroup]=relationship(cascade='save-update,merge,delete')
-    __mapper_args__ = {
-        "polymorphic_identity": "template",
-        "polymorphic_on": "type",
-    }
-
-class DemandTemplate(Base):
-    "template of demands (ie uncovered duties)"
-    __tablename__="demand_template"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    ruleset_id:Mapped[int]=mapped_column(ForeignKey('rule.id'))
-    ruleset:Mapped[RuleGroup]=relationship(cascade='save-update,merge,delete')
-    name:Mapped[str]
-    activity_tags:Mapped[str]
-    start_time:Mapped[datetime.datetime]
-    finish_time:Mapped[datetime.datetime]
+class DemandTemplate(BaseModel):
+    id:uuid.UUID=Field(default_factory=uuid.uuid4)
+    ruleset:RuleGroup
+    name:str
+    activity_tags:set[str]
+    start_time:datetime.time
+    duration:datetime.timedelta
     def matches(self,date):
         return self.ruleset.matches(date)
-    @classmethod
-    def from_json(cls,data):
-        data['start_time']=datetime.datetime.fromisoformat(data['start_time'])
-        data['finish_time']=datetime.datetime.fromisoformat(data['finish_time'])
-        data['rules']=Rule.from_json(data['rules'])
-        return cls(**data)
     
 
-class SupplyTemplateEntry(Base):
-    __tablename__="supply_template_entry"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    dateoffset:Mapped[int]
-    activity_tags:Mapped[str]
-    template_id:Mapped[int]=mapped_column(ForeignKey('supply_template.id'))
+class SupplyTemplateEntry(BaseModel):
+    dateoffset:int
+    activity_tags:set[str]
+    template_id:uuid.UUID
     
-class SupplyTemplate(Base):
+class SupplyTemplate(BaseModel):
     "template of offers of cover"
-    __tablename__="supply_template"
-    staff:Mapped[str]
-    ruleset_id:Mapped[int]=mapped_column(ForeignKey('rule.id'))
-    ruleset:Mapped[RuleGroup]=relationship(cascade='save-update,merge,delete')
-    name:Mapped[str]
-    entries:Mapped[list[SupplyTemplateEntry]]=relationship(cascade='save-update,merge,delete')
+    id:uuid.UUID=Field(default_factory=uuid.UUID4)
+    staff:str
+    ruleset:RuleGroup
+    name:str
+    entries:list[SupplyTemplateEntry]
     def materialise(self,date,activities:dict[datetime.date,list['Activity']]):
         results=[]
         if not self.ruleset.matches(date):
@@ -238,16 +172,6 @@ class SupplyTemplate(Base):
                 yield combi
 
 
-
-rule_types={
-    RuleType.DAILY:DailyRule,
-    RuleType.WEEKLY:WeeklyRule,
-    RuleType.MONTHLY:MonthlyRule,
-    RuleType.WEEK_IN_MONTH:WeekInMonthRule,
-    RuleType.DATE_RANGE:DateRangeRule,
-    RuleType.DATE_TAGS:DateTagsRule,
-    RuleType.GROUP:RuleGroup
-}
 
 
 def generate_offers_for_range(start,finish):
@@ -316,21 +240,12 @@ def rule_matches(date:datetime.date, rule:Rule):
 def get_all_templates():
     return demand_templates.values()
 
-from sqlalchemy import create_engine,select
-engine = create_engine("sqlite://", echo=True)
-Base.metadata.create_all(engine)
+rulegroup=RuleGroup(group_type="and")
+daily_rule=DailyRule(day_interval=1,anchor_date=datetime.datetime.today().date())
+rulegroup.members.append(daily_rule)
+template=DemandTemplate(name='x',ruleset=rulegroup,start_time=datetime.datetime.today().time(),duration='P1DT1H59M59S',activity_tags={'1','2','3'})
+print (template)
+template_str=template.model_dump_json()
+print(DemandTemplate.model_validate_json(template_str))
 
-from sqlalchemy.orm import Session
-
-with Session(engine) as session:
-    rulegroup=RuleGroup(group_type="and")
-    daily_rule=DailyRule(day_interval=1,anchor_date=datetime.datetime.today())
-    rulegroup.members.append(daily_rule)
-    template=DemandTemplate(name='x',start_time=datetime.datetime.today(),finish_time=datetime.datetime.today(),activity_tags="")
-    template.ruleset=rulegroup
-    session.add(template)
-    session.commit()
-    for row in session.scalars(select(Rule)):
-        print(row)
-    session.delete(template)
-    session.commit()
+print(json.dumps(template.model_json_schema(),indent=2))
