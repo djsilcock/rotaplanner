4f3ff8d7c7bd63e4a906cf8b4aa03fe15f9823ef
djsilcock <djsilcock@users.noreply.github.com>
2024-09-04 07:50:44 +0000

    .

--- py_rotaplanner/static/table.js
+++ py_rotaplanner/static/table.js
@@ -1,17 +1,24 @@
 document.addEventListener('DOMContentLoaded',()=>{
     const table=document.getElementById("rota-table")
+    let draglimit=""
     table.addEventListener('dragstart',e=>{
-        e.dataTransfer.setData('text/plain',e.target.id)
-        console.log('dragging',e.target)
+        const payload=JSON.stringify({
+            date:e.target.dataset.activitydate,
+            activityId:e.target.dataset.activityid
+        })
+        e.dataTransfer.setData('text/plain',payload)
+        draglimit=e.target.dataset.activitydate
+        console.log('dragging',e.target,draglimit)
     })
     table.addEventListener('dragend',e=>{
         document.querySelectorAll('.activitycell').forEach(cell=>{
             cell.classList.remove('dragover')
+            draglimit=""
         })
     })
     document.querySelectorAll('.activitycell').forEach(cell=>{
         cell.addEventListener('dragover',e=>{
-            e.preventDefault()
+            if (draglimit==cell.dataset.date) e.preventDefault()
         })
         cell.addEventListener('dragenter',e=>{
             cell.classList.add('dragover')
@@ -23,6 +30,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     table.addEventListener('drop',e=>{
         console.log('attempt to drop ',e.dataTransfer.getData('text/plain'),'into',e.target.id)
         const droptarget=e.target
+        draglimit=""
         const activity=document.getElementById(e.dataTransfer.getData('text/plain'))
         console.log(droptarget,activity)
         const newdiv=document.createElement('div')
